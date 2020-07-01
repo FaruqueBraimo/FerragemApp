7496fb54c1fd8e0f89bdaf7631ba65cb315134a9
FaruqueBraimo <faruquebraimo@gmail.com>
2020-07-01 14:14:29 +0200

    implementando fatura e a UI

---
+++ src/components/sales/invoice/invoiceComponent.vue
@@ -0,0 +1,217 @@
+<template>
+	<div class="q-pb-md ">
+          
+ <div class="q-py-md" >
+   <q-input filled v-model="date" placeholder='Prazo de Validade' dense>
+      <template v-slot:append>
+        <q-icon name="event" class="cursor-pointer">
+          <q-popup-proxy transition-show="scale" transition-hide="scale">
+            <q-date v-model="date" mask="DD-MM-YYYY "  />
+          </q-popup-proxy>
+        </q-icon>
+      </template>
+	  
+	  </q-input>
+  </div>
+
+		<div class="" style="">
+			{{ fetchClients }}
+			<q-card square class="">
+				<q-tabs
+					v-model="tab"
+					dense
+					class="text-grey"
+					active-color="primary"
+					indicator-color="primary"
+					align="justify"
+					narrow-indicator
+				>
+					<q-tab name="mails" label="Detalhes da Factura" />
+					<q-tab name="alarms" label="Cliente" />
+				</q-tabs>
+
+				<q-separator />
+
+				<q-tab-panels v-model="tab" animated>
+					<q-tab-panel name="mails">
+						<q-card
+							class="my-card bg-deep-orange-1
+"
+							flat
+							bordered
+							square
+						>
+                        	
+							<div class="row q-pa-sm text-deep-orange-5 ">
+								<div class="col">Subtotal :</div>
+								<div class="col text-right q-pr-md">
+									{{ getSubTotal }} ,00 MT
+								</div>
+							</div>
+							<div class="row q-pa-sm text-deep-orange-5 ">
+								<div class="col-6 text-left">
+									Desconto de Iva :
+								</div>
+								<div class="col text-right q-pr-md">
+									0,00 MT
+								</div>
+							</div>
+
+							<div class="row q-pa-sm text-deep-orange-5">
+								<div class="col">Total :</div>
+								<div class="col text-right q-pr-md">
+									{{ getSubTotal }} ,00 MT
+								</div>
+							</div>
+
+							<div class="row q-pa-sm text-deep-orange-5">
+								<div class="col">Prazo :</div>
+								<div class="col text-right q-pr-md">
+									{{ date || 'Por Preencher'}} 
+								</div>
+							</div>
+
+
+							<div class="row q-pa-sm text-deep-orange-5">
+								<div class="col">Cliente :</div>
+								<div class="col text-right q-pr-md">
+									{{ client.label || 'Não Informado' }} 
+								</div>
+							</div>
+
+
+
+						</q-card>
+					</q-tab-panel>
+
+					<q-tab-panel name="alarms">
+						<q-form
+							@submit="onSubmit"
+							@reset="onReset"
+							class="q-gutter-md"
+						>
+							<div class="row q-px-sm">
+								<!-- <p class="text-h6 text-primary text-bold"> Detalhes do producto </p> -->
+
+								<div class="col-12 q-px-sm">
+									<q-select
+										dense
+										label=" Selecione o Cliente"
+										square
+										filled
+										:options="optionalClient"
+										v-model="client"
+									/>
+								</div>
+
+							
+							</div>
+
+							<div class="row  items-center">
+									<div class=" col-12 text-primary q-pl-sm  ">
+									
+									<q-btn
+                                    no-caps
+										label="Ou registe um novo cliente"
+										type="reset"
+										color="primary"
+										flat
+										class="q-ml-sm text-center full-width"
+									/>
+									
+								</div>
+								
+							</div>
+						</q-form>
+					</q-tab-panel>
+				</q-tab-panels>
+			</q-card>
+		</div>
+		<div class="row q-pa-lg">
+		<div class="q-pt-xl col-12">
+				<q-btn
+					color="indigo"
+					no-caps
+					class=" full-width"
+					unelevated
+					icon="done"
+					label="Finalizar"
+					:disable="disable"
+					@click="$emit('invoice',{  subtotal : getSubTotal, iva:0, client :client , deadline : date})"
+
+				/>
+
+			</div>
+
+		
+		</div>
+	</div>
+</template>
+
+<script>
+	import { mapActions, mapState, mapGetters } from 'vuex';
+	import { date } from 'quasar';
+
+	export default {
+		data() {
+			return {
+				tab: 'mails',
+				client: '',
+                optionalClient: [],
+                value : 0,
+				change : 0,
+				disable:true,
+			    date: ''
+
+
+
+			};
+		},
+		computed: {
+			...mapGetters('checkedProduct', ['getSubTotal']),
+			...mapState('customer', ['customers']),
+
+			fetchClients() {
+			
+			const dateReturn = date.extractDate(this.date, 'DD-MM-YYYY ')
+            const dateNow = new Date
+			  if(this.date !='' && dateReturn < dateNow) {
+					   this.disable = true 
+					   
+					this.$q
+					.dialog({
+						title: 'Data Inválida',
+						message: `A data Introduzida é inferior a data actual. 
+						`,
+						ok: 'Sim',
+					})
+			  }
+			  else {
+				 this.disable = false 	
+			  }
+
+				Object.keys(this.customers).forEach((element, key) => {
+					this.optionalClient.push({
+						value: element,
+						label: this.customers[element].name
+					});
+				});
+				return '';
+			}
+		},
+
+		methods: {
+			onSubmit() {},
+            onReset() {},
+            finalCheck() {
+
+
+            }
+		},
+
+		mounted() {
+		
+		}
+	};
+</script>
+
---
+++ src/store/modules/invoices.js
@@ -0,0 +1,200 @@
+import Vue from 'vue';
+import { dbInvoices } from '../../boot/firebase';
+import { Loading, date } from 'quasar';
+import { showErrorMessage } from '../../functions/handle-error-messages';
+import { showSuccessMessage } from '../../functions/show-success-messages';
+
+let lastVisible = null;
+
+const state = {
+	invoices: {},
+	uploadProgress: 0,
+	loading: false,
+	invoicesearchKey: ''
+};
+
+const mutations = {
+	loading(state, val) {
+		state.loading = val;
+	},
+
+	uploadProgress(state, val) {
+		state.uploadProgress = val;
+	},
+	addInvoice(state, payload) {
+		Vue.set(state.invoices, payload.id, payload.object);
+	},
+
+	editInvoice(state, payload) {
+		Object.assign(state.invoices[payload.id], payload.updates);
+	},
+	deleteInvoice(state, id) {
+		Vue.delete(state.invoices, id);
+	}
+};
+
+const getters = {};
+
+const actions = {
+	listeninvoiceRealTimeChanges({ commit }) {
+		commit('resetInvoice');
+
+		dbInvoices
+			.orderBy('createdAt', 'desc')
+			.limit(10)
+			.onSnapshot(function(snapshot) {
+				snapshot.docChanges().forEach(function(change) {
+					if (change.type === 'added') {
+						commit('addInvoice', {
+							id: change.doc.id,
+							object: change.doc.data()
+						});
+					}
+					if (change.type === 'modified') {
+						commit('editInvoice', {
+							id: change.doc.id,
+							updates: change.doc.data()
+						});
+					}
+					if (change.type === 'removed') {
+						commit('deleteInvoice', change.doc.id);
+					}
+				});
+			});
+	},
+
+	addInvoice({ commit, dispatch, rootGetters }, payload) {
+		payload.createdAt = new Date();
+		payload.updatedAt = new Date();
+		commit('loading', true);
+
+		return dbInvoices
+			.add(payload)
+			.then(docRef => {
+				commit('loading', false);
+
+				// 1. Limpar todas solicitações
+				commit('resetInvoice');
+
+				showSuccessMessage('Factura Emitida!');
+
+				return true;
+			})
+			.catch(function(error) {
+				console.error('Error adding document: ', error);
+				commit('loading', false);
+				showErrorMessage(error.message);
+
+				return false;
+			});
+	},
+
+	editInvoice({ commit, rootGetters }, payload) {
+		commit('loading', true);
+
+		payload.updates.updatedAt = new Date();
+
+		let sucessMessage = payload.successMessage
+			? payload.successMessage
+			: state.defaultUpdateSuccessMessage;
+
+		return dbInvoices
+			.doc(payload.id)
+			.update(payload.updates)
+			.then(function(docRef) {
+				commit('loading', false);
+
+				showSuccessMessage(' Factura  Editada!');
+
+				return true;
+			})
+			.catch(function(error) {
+				console.error('Error adding document: ', error);
+				commit('loading', false);
+
+				showErrorMessage(error.message);
+
+				return false;
+			});
+	},
+
+	deleteInvoice({ commit }, id) {
+		return dbInvoices
+			.doc(id)
+			.delete()
+			.then(function(docRef) {
+				showSuccessMessage(' Eliminada com sucesso...');
+				return true;
+			})
+			.catch(function(error) {
+				console.error('Error removing document: ', error);
+				commit('loading', false);
+				showErrorMessage(error.message);
+				return false;
+			});
+	},
+
+	getInvoiceByUserId({ commit }, userId) {
+		if (!userId) {
+			showErrorMessage(
+				'Algumas informações das solicitações que deseja visualizar, poderão não ser visualizadas neste momento. Favor favor, tente mais tarde.'
+			);
+			return;
+		}
+
+		commit('loading', true);
+
+		return dbInvoices
+			.where('userId', '==', userId)
+			.orderBy('createdAt', 'desc')
+			.limit(50)
+			.get()
+			.then(resp => {
+				commit('loading', false);
+
+				let invoices = [];
+				resp.docChanges().forEach(item => {
+					let invoices = item.doc.data();
+					invoices.id = item.doc.id;
+
+					invoices.push(invoices);
+				});
+				commit('loading', false);
+
+				return invoices;
+			})
+			.catch(function(error) {
+				console.log('Error ocured: ', error);
+				commit('loading', false);
+				return null;
+			});
+	},
+
+	setInvoicesearchKey({ commit, dispatch }, text) {
+		commit('setInvoicesearchKey', text);
+
+		commit('loading', true);
+		commit('resetInvoice');
+
+		if ((text && text.length > 1) || !text) {
+			dispatch('getData', true);
+		}
+	},
+
+	resetDataToOnly20({ state, commit }) {
+		if (Object.keys(state.invoices).length > 75) {
+			console.log(
+				'Reset data to only20 not implemented yet. All invoices where reseted for now...'
+			);
+			commit('resetInvoice');
+		}
+	}
+};
+
+export default {
+	namespaced: true,
+	state,
+	mutations,
+	getters,
+	actions
+};
